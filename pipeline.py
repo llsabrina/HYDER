9c8eeb5351b00657eb0b6cc50fbe0a330df80909
llsabrina <sabrinaalessandra2155@gmail.com>
2026-07-21 00:45:49 -0300

    feat: adicionar análise de status e mensalidade por filtro

--- pipeline.py
+++ pipeline.py
@@ -16,9 +16,25 @@ try:
         save_relatorio.append(df_alunos)
         logging.info("Arquivo processado: %s", arquivo)
 
-    pd.concat(save_relatorio).to_csv("C:/Users/sabri/Desktop/up level/python/hyder/hyder_relatorio.csv", index=False)
-    print(len(pd.concat(save_relatorio)), "linhas processadas e salvas no arquivo hyder_relatorio.csv")
+    df_final = pd.concat(save_relatorio)
+    df_final.to_csv("C:/Users/sabri/Desktop/up level/python/hyder/hyder_relatorio.csv", index=False)
+    print(len(df_final), "linhas processadas e salvas no arquivo hyder_relatorio.csv")
     logging.info("Relatório gerado com sucesso!")
+
+    df_ativos = df_final[df_final["status"] == "ativo"]
+    mensalidade_ativos = df_ativos["mensalidade"].sum()
+    total_ativos = len(df_ativos)
+
+    logging.info("Total da mensalidade dos alunos ativos: %d", mensalidade_ativos)
+    logging.info("Total de alunos ativos: %d", total_ativos)
+
+    df_desistentes = df_final[df_final["status"] == "desistente"]
+    total_desistentes = len(df_desistentes)
+    logging.info("Total de alunos desistentes: %d", total_desistentes)
+    df_trancados = df_final[df_final["status"] == "trancado"]
+    total_trancados = len(df_trancados)
+    logging.info("Total de alunos trancados: %d", total_trancados)
+
 except Exception as e:
     logging.error("Erro ao processar arquivos: %s", str(e))
 
